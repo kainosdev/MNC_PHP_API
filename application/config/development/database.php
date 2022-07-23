20f0b2b9bd9067db5b9bc409cdc2e283ab84d809
kainosdev <kainosdevelopmentteam@gmail.com>
2022-07-23 13:17:02 +0530

    abhi

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'DESKTOP-L8DUMLV\SQLEXPRESS',
-	'username' => '',
-	'password' => '',
-	'database' => 'jul4-2022-vms',
+	'hostname' => 'DESKTOP-9P2PCN0\SQL_SERVER',
+	'username' => 'sa',
+	'password' => 'Abirami',
+	'database' => 'jul18-2022 vms',
 	'dbdriver' => 'sqlsrv',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
